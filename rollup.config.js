8b0802774cdf8cd74849214a3e735143342c6581
Kingller Tang <kingller.tang@gaiaworks.cn>
2023-10-24 16:35:40 +0800

    将文件编译成es5,支持ie11

--- rollup.config.js
+++ rollup.config.js
@@ -8,30 +8,36 @@ import ts from 'rollup-plugin-typescript2';
 import alias from 'rollup-plugin-alias';
 
 const plugins = [
-  cleanup(),
-  json(),
-  nodeResolve(),
-  ts({
-    tsconfig: path.resolve(__dirname, './tsconfig.json'), // 导入本地ts配置
-    clean: true,
-    useTsconfigDeclarationDir: true,
-  }),
-  commonjs(),
-  alias({
-    entries: [{ find: '@', replacement: './lib' }],
-  }),
-  terser(),
+    cleanup(),
+    json(),
+    nodeResolve(),
+    ts({
+        tsconfig: path.resolve(__dirname, './tsconfig.json'), // 导入本地ts配置
+        clean: true,
+        useTsconfigDeclarationDir: true,
+        tsconfigOverride: {
+            compilerOptions: {
+                target: 'es5',
+                downlevelIteration: true,
+            },
+        },
+    }),
+    commonjs(),
+    alias({
+        entries: [{ find: '@', replacement: './lib' }],
+    }),
+    terser(),
 ];
 
 module.exports = {
-  input: path.resolve('./lib/index.ts'),
-  output: [
-    {
-      exports: 'auto',
-      file: path.resolve(__dirname, './dist/index.js'),
-      format: 'umd',
-      name: 'pinyinPro',
-    },
-  ],
-  plugins,
+    input: path.resolve('./lib/index.ts'),
+    output: [
+        {
+            exports: 'auto',
+            file: path.resolve(__dirname, './dist/pinyin-pro.min.js'),
+            format: 'umd',
+            name: 'pinyinPro',
+        },
+    ],
+    plugins,
 };
